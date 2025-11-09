bc7794ccc988078237d5e4b1823e49230ad6f244
Sidhesh795 <heysidek@gmail.com>
2025-11-09 21:14:45 +0530

    Ready for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import TOCFlashcards from './toc_rev.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TOCFlashcards />
+  </StrictMode>
+);
